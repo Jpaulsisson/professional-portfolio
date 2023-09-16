61894c475080bdbd611f0d5cc7f2ba3631d64708
Paul Sisson <paulsissonsemail@gmail.com>
2023-09-16 14:05:06 -0500

    Add nice comment border and timestamp formatting in comments

--- src/app/utils/supabase.js
+++ src/app/utils/supabase.js
@@ -6,4 +6,4 @@ const url = env.SUPABASE_URL
 const key = env.SUPABASE_KEY
 
 // Create a single supabase client for interacting with your database
-export const supabase = createClient("https://nkzirimhwoydzdjvjzjd.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5remlyaW1od295ZHpkanZqempkIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQ3ODkwNTgsImV4cCI6MjAxMDM2NTA1OH0.tqBP-3FbF_9EeXX9m5cYxq3ntwjHoMHRBmegOVKoEH4")
+export const supabase= createClient("https://nkzirimhwoydzdjvjzjd.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5remlyaW1od295ZHpkanZqempkIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQ3ODkwNTgsImV4cCI6MjAxMDM2NTA1OH0.tqBP-3FbF_9EeXX9m5cYxq3ntwjHoMHRBmegOVKoEH4")
