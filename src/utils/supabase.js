5f0785d62cd1eff3a111799691767d548628737b
Paul Sisson <paulsissonsemail@gmail.com>
2023-09-28 07:25:25 -0500

    Fix env variables

--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const url = process.env.SUPABASE_URL
-const key = process.env.SUPABASE_KEY
+const url = process.env.NEXT_PUBLIC_SUPABASE_URL
+const key = process.env.NEXT_PUBLIC_SUPABASE_KEY
 
 // Create a single supabase client for interacting with your database
 export const supabase= createClient(url, key);
