a3a145b2791db900ec1de25200385ee5c8695c24
Paul Sisson <paulsissonsemail@gmail.com>
2024-01-31 08:23:55 -0600

    See if making photos async improves load time

--- src/components/photo-grid/photo-grid.component.tsx
+++ src/components/photo-grid/photo-grid.component.tsx
@@ -5,21 +5,21 @@ import KidOne from '../../resources/kid-one.jpg'
 import KidTwo from '../../resources/kid-two.jpg'
 import Bday from '../../resources/muhbday.jpeg'
 
-export default function PhotoGrid() {
+export default async function PhotoGrid() {
   return (
     <section className='photo-grid w-4/5 mb-10 grid items-center justify-center grid-cols-12 grid-rows-5 rounded-md'>
-    <div className=" col-start-2 col-span-6 row-start-1 row-span-3">
-      <Image className='rounded-sm' src={Wifey} alt='my hot wife' />
-    </div>
-    <div className=" col-start-9 col-span-3 row-start-1 row-span-3">
-      <Image className='rounded-sm' src={KidOne} alt='my son' />
-    </div>
-    <div className=' col-start-8 col-span-3 row-start-3 row-span-2' >
-      <Image className='rounded-sm' src={KidTwo} alt='my daughter' />
-    </div>
-    <div className=' col-start-3 col-span-4 row-start-3 row-span-3'>
-      <Image className='rounded-sm ' src={Bday} alt='my birthday' />
-    </div>      
-  </section>
+      <div className=" col-start-2 col-span-6 row-start-1 row-span-3">
+        <Image className='rounded-sm' src={Wifey} alt='my hot wife' />
+      </div>
+      <div className=" col-start-9 col-span-3 row-start-1 row-span-3">
+        <Image className='rounded-sm' src={KidOne} alt='my son' />
+      </div>
+      <div className=' col-start-8 col-span-3 row-start-3 row-span-2' >
+        <Image className='rounded-sm' src={KidTwo} alt='my daughter' />
+      </div>
+      <div className=' col-start-3 col-span-4 row-start-3 row-span-3'>
+        <Image className='rounded-sm ' src={Bday} alt='my birthday' />
+      </div>
+    </section>
   )
 }
